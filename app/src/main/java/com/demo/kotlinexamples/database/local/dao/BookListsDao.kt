db98cd6d32ddca928980a4d8b5853d1d9d24d9c9
Radhika-Patel93 <radhikadpatel20@gmail.com>
2020-10-04 15:50:06 +0530

    Added Firebase data to Local Database.

--- app/src/main/java/com/demo/kotlinexamples/database/local/dao/BookListsDao.kt
+++ app/src/main/java/com/demo/kotlinexamples/database/local/dao/BookListsDao.kt
@@ -1,13 +1,12 @@
 package com.demo.kotlinexamples.database.local.dao
 
-import androidx.room.Dao
-import androidx.room.Query
-import androidx.room.RawQuery
+import androidx.room.*
 import androidx.sqlite.db.SupportSQLiteQuery
 import com.demo.kotlinexamples.data.database.common.entity.BookListsEntity
 import com.demo.kotlinexamples.data.database.local.dao.LocalDao
 import io.reactivex.Completable
 import io.reactivex.Flowable
+import io.reactivex.Maybe
 import io.reactivex.Single
 
 
@@ -24,4 +23,10 @@ abstract class BookListsDao : LocalDao<BookListsEntity>(
 
     @Query("DELETE FROM number_lists")
     abstract override fun deleteAll(): Completable
+
+    @Query("SELECT * FROM book_lists")
+    abstract fun getData(): List<BookListsEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertData(vararg users: BookListsEntity)
 }
--- app/src/main/java/com/demo/kotlinexamples/firebase/Realtime_Database.kt
+++ app/src/main/java/com/demo/kotlinexamples/firebase/Realtime_Database.kt
@@ -2,23 +2,21 @@ package com.demo.kotlinexamples.firebase
 
 import android.content.Context
 import androidx.room.Room.databaseBuilder
+import com.demo.kotlinexamples.data.database.common.entity.BookListsEntity
+import com.demo.kotlinexamples.data.database.local.PhoneicCacheDatabase
 import com.demo.kotlinexamples.data.database.remote.firebase.PhoneicFirebaseDatabase
-import com.demo.kotlinexamples.data.database.remote.firebase.numberlists.FbBookLists
-import com.demo.kotlinexamples.data.database.remote.firebase.numberlists.FbBookListsDao
-import com.demo.kotlinexamples.util.logd
+import com.demo.kotlinexamples.data.database.remote.firebase.base.toFbMap
 import com.google.firebase.database.*
 import com.google.gson.Gson
-import io.reactivex.BackpressureStrategy
-import io.reactivex.Flowable
-import io.reactivex.Observable
+import com.google.gson.reflect.TypeToken
 
 
 object Realtime_Database {
 
-    fun getBookList(){
+    fun getBookList(context: Context){
 
         var dbMainRef = PhoneicFirebaseDatabase.bookLists
-        dbMainRef.ref.addValueEventListener(object : ValueEventListener{
+        dbMainRef.ref.addValueEventListener( object : ValueEventListener{
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
@@ -28,80 +26,53 @@ object Realtime_Database {
                 children.forEachIndexed { index, it ->
                     println("Key : " + it.key)
                     println("Value : " + it.value)
-                }
 
+                    initDatabase(context)
+                    saveDataInDatabase(context, index, it.key.toString(), Gson().toJson(it.value))
+                }
+                  fetchAllData(context)
             }
 
         })
 
     }
 
+//    Database functions;
+    fun initDatabase(context: Context): PhoneicCacheDatabase {
+        val db = databaseBuilder(context, PhoneicCacheDatabase::class.java, "todo.db").build()
+        return db
+    }
+
+    fun saveDataInDatabase(context: Context, id: Int, key: String, value: String) {
+        val db = initDatabase(context)
+        val thread = Thread {
+            var todoEntity = BookListsEntity(id.toString(), 0,0,key, value)
+            db.bookListsDao().insertData(todoEntity)
+        }
+        thread.start()
+    }
+
+    fun fetchAllData(context: Context) {
+        val thread = Thread {
+            val db = initDatabase(context)
+            //fetch Records
+            db.bookListsDao().getData().forEach()
+            {
+               StringToJson(Gson().fromJson(it.rawData, object : TypeToken<Map<String, Any>>() {}.type))
+            }
+        }
+        thread.start()
+
+    }
+
+    fun StringToJson(fromJson: Any) {
+
+        fromJson.toFbMap()?.forEach(){
+            println("Fetch Key: "+ it.key)
+            println("Fetch Value: "+ it.value)
+        }
+    }
+
 }
-
-
-//    fun getAllData(context: Context) {
-//        val rootRef = FirebaseDatabase.getInstance().reference.child("/stagging/data")
-//        rootRef.addValueEventListener(object : ValueEventListener {
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//                val children = snapshot.children
-//                children.forEachIndexed { index, it ->
-//                    println("Key : " + it.key)
-//                    println("Value : " + it.value)
-//
-//                    initDatabase(context)
-//                    saveDataInDatabase(context, index, it.key.toString(), Gson().toJson(it.value))
-////                    StringToJson(Gson().toJson(it.value))
-//                }
-//
-//                fetchAllData(context)
-//            }
-//
-//            override fun onCancelled(error: DatabaseError) {
-//                println(error.message)
-//            }
-//        })
-//    }
-//
-//    //Database functions;
-//    fun initDatabase(context: Context): AppDatabase {
-//        val db = databaseBuilder(context, AppDatabase::class.java, "todo.db").build()
-//        return db
-//    }
-//
-//    fun saveDataInDatabase(context: Context, id: Int, key: String, value: String) {
-//        val db = initDatabase(context)
-//        val thread = Thread {
-//            var todoEntity = TodoEntity(id, key, value)
-//            db.todoDao().insertAll(todoEntity)
-//        }
-//        thread.start()
-//    }
-//
-//    fun fetchAllData(context: Context) {
-//        val thread = Thread {
-//            val db = initDatabase(context)
-//            //fetch Records
-//            db.todoDao().getAll().forEach()
-//            {
-//                println("Fetch Records Id:  : ${it.id}")
-//                println("Fetch Records Key:  : ${it.key}")
-//                println("Fetch Records Value:  : ${it.value}")
-//            }
-//
-//            println("Fetch Records:  : ${db.todoDao().getAll().size}")
-//        }
-//        thread.start()
-//
-//    }
-
-//    fun StringToJson(data: String) {
-//        val gson = Gson()
-//        val mapType = object : TypeToken<Map<String, Any>>() {}.type
-//
-//        var tutorialMap: Map<String, Any> = gson.fromJson(data, object : TypeToken<Map<String, Any>>() {}.type)
-//        tutorialMap.forEach {
-//            println("StringToJson: "+it.key+"  ||  "+it.value)
-//        }
-//    }
 
 
